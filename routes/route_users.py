c047e2f5ed6ed7420662b0fc401eb3237f9e7b81
David Naki <dlnaki1160@gmail.com>
2025-05-06 14:33:58 +0300

    add readme.md

--- routes/route_users.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-users_router = Blueprint('users', __name__, url_prefix='/users')
-
-
-@users_router.route('')
-def get_users():
-    return "Now you need to get a list of all users"
